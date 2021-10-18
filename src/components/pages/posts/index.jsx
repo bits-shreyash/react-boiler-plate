d39a67e46e41bb0320281d21fe7900dd61468f9a
Shreyash <shreyashwadgaonkar@benchmarkit.solutions>
2021-10-18 12:26:43 +0530

    basic changes

--- src/components/pages/posts/index.jsx
+++ src/components/pages/posts/index.jsx
@@ -3,14 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchPostList } from '../../../redux/posts/action';
 import PageTemplate from '../../templates/page';
 import Posts from '../../organisms/post'
-
+/**
+ * Method: PostsPage
+ * Description: This component 
+ * @returns 
+ */
 const PostsPage = ()=> {
 
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(fetchPostList())
   },[]);
-
   const data = useSelector(state => state.posts.posts) || {};
   return (
     <PageTemplate>
